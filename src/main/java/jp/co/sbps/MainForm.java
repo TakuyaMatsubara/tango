a76eb8109c7faf164419f7a09006a1ed08a1d1ee
tamatsubara <tamatsubara@3162-7.intra.softbank.co.jp>
2016-07-19 12:44:42 +0900

    バグの修正

--- src/main/java/jp/co/sbps/MainForm.java
+++ src/main/java/jp/co/sbps/MainForm.java
@@ -1,15 +1,13 @@
 package jp.co.sbps;
 
-import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class MainForm implements Serializable{
+public class MainForm  {
 	
-	private static final long serialVersionUID = -3201728685446988505L;
-
+	
 	
 	private List<RequestForm> requests;
 	
--- src/main/java/jp/co/sbps/RequestForm.java
+++ src/main/java/jp/co/sbps/RequestForm.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class RequestForm   {
 
-//	private static final long serialVersionUID = -3201728685446988505L;
+
 	
-//	private String  questionId;
+	private String  questionId;
 	
 	private String answer;
 	
-//	private String points;
+	//private String points;
 		
 }
